9487fd6958c41f24c7ee5f0cf6119141db24c25b
sing1249 <sing1249@algonquinlive.com>
2024-11-29 17:46:54 -0500

    Making changes to the design

--- src/components/TopNav.vue
+++ src/components/TopNav.vue
@@ -2,7 +2,7 @@
   <nav>
     <div class="logo">
       <a href="/">
-        <img src="/algonquin-pet-store.png" alt="Algonquin Pet Store Logo">
+        <img src="/bestbuylogo.jpg" alt="Best Buy Logo">
       </a>
     </div>
     <button class="hamburger" @click="toggleNav">
